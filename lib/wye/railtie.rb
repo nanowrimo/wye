1585090cfe63e3f606433392138eacc9f3e78f87
Daniel Duvall <dan@mutual.io>
2012-11-08 13:18:16 -0800

    Added AR base extension to railtie.

--- lib/wye/railtie.rb
+++ lib/wye/railtie.rb
@@ -5,6 +5,7 @@ module Wye
   class Railtie < Rails::Railtie
     initializer 'wye.set_connection_handler', :before => 'active_record.initialize_database' do
       ActiveSupport.on_load(:active_record) do
+        require 'wye/active_record/base'
         ::ActiveRecord::Base.connection_handler = Wye::ActiveRecord::ConnectionHandler.new
       end
     end
